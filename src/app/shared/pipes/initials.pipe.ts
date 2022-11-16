58b0d3a7b3baaae89f1b155c09038d11db7efea3
AgathePons <ponsagathe37@gmail.com>
2022-11-16 11:54:07 +0100

    UPDATE: pipe for initials of name improved

--- src/app/shared/pipes/initials.pipe.ts
+++ src/app/shared/pipes/initials.pipe.ts
@@ -7,14 +7,27 @@ import { Stagiaire } from 'src/app/core/models/stagiaire';
 export class InitialsPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    if(value instanceof Stagiaire) {
-      return this.getInitials(value).toUpperCase();
+
+    if (value instanceof Stagiaire) {
+      return this.getInitials(value, args).toUpperCase();
     } else {
-      throw new Error('value is not a Stagiaire object')
+      throw new Error('value is not a Stagiaire object');
     }
   }
 
-  private getInitials(stagiaire: Stagiaire): string {
+  private getInitials(stagiaire: Stagiaire, variation: unknown[]): string {
+    const variant: any = variation[0];
+    if (variant !== undefined && variant.firstNameFirst === false) {
+      return this.lastNameFirst(stagiaire);
+    }
+    return this.firstNameFirst(stagiaire);
+  }
+
+  private firstNameFirst(stagiaire: Stagiaire): string {
     return stagiaire.getFirstName().charAt(0) + stagiaire.getLastName().charAt(0);
+  }
+
+  private lastNameFirst(stagiaire: Stagiaire): string {
+    return stagiaire.getLastName().charAt(0) + stagiaire.getFirstName().charAt(0);
   }
 }
