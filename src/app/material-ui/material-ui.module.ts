9a73ca7be35346a3958ad661500c649242484b4b
AgathePons <ponsagathe37@gmail.com>
2022-11-28 14:19:01 +0100

    add: form service

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepicker } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [],
@@ -22,5 +23,6 @@ export class MaterialUiModule {
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+
   ]
 }
