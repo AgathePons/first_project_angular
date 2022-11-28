f0e41cf2033021166afb46f0c428733a2078dca9
AgathePons <ponsagathe37@gmail.com>
2022-11-28 12:26:44 +0100

    upate: html form with material WIP

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -4,15 +4,23 @@ import { CommonModule } from '@angular/common';
 // Forms UI from material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ...MaterialUiModule.materialModules
   ]
 })
 export class MaterialUiModule {
   public static materialModules = [
-
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 }
