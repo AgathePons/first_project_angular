3dc3500d7b56550d6ff42398c7c48b5434fb4249
AgathePons <ponsagathe37@gmail.com>
2022-11-28 17:06:13 +0100

    add: package to handle date format + WIP

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDatepicker } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [],
@@ -23,6 +23,6 @@ export class MaterialUiModule {
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-
+    MatDatepickerModule,
   ]
 }
