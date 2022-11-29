ee7cb7d81ed9744eb4bb23c604307674a59e78a3
AgathePons <ponsagathe37@gmail.com>
2022-11-29 09:35:39 +0100

    add: routing WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { StagiaireTableComponent } from './stagiaires/components/stagiaire-table/stagiaire-table.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(AppRoutingModule.routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  public static routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'home', // redirection to another path
+      pathMatch: 'full', // check all the route (not just one section)
+    },
+    {
+      path: 'home',
+      component: StagiaireTableComponent,
+    },
+    // must be the last route of the list
+    {
+      path: '**', // wild card
+      redirectTo: 'home',
+      pathMatch: 'full',
+    }
+  ];
+}
