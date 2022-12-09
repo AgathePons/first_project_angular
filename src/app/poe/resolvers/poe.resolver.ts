49432aa9b1cba32b812c3a14ef25ab1cc1a63bc5
AgathePons <ponsagathe37@gmail.com>
2022-12-09 10:05:37 +0100

    resolver wrks but just add

--- src/app/poe/resolvers/poe.resolver.ts
+++ src/app/poe/resolvers/poe.resolver.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import {
   Router, Resolve,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
+  ActivatedRoute
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { Poe } from 'src/app/core/models/poe';
+import { PoeService } from 'src/app/core/service/poe.service';
+import { FormBuilderService } from 'src/app/shared/form-builder.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PoeResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+export class PoeResolver implements Resolve<FormGroup> {
+
+  public constructor(
+    private route: ActivatedRoute,
+    private poeService: PoeService,
+    private formBuilderService: FormBuilderService,
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FormGroup> {
+    const id: number = +route.paramMap.get('id')!;
+    console.log('poe Resolver >> got id:', id);
+    let poe: Poe;
+    let form: FormGroup;
+    if (id === 0) {
+      poe = new Poe();
+      form = this.formBuilderService.buildPoe(poe).getForm();
+      return of(form);
+    } else {
+      // TODO resolver for update
+      poe = new Poe();
+      form = this.formBuilderService.buildPoe(poe).getForm();
+      return of(form);
+    }
+
   }
+
 }
