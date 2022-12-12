7e703e1d12765e0331413c1edc6a93e3ea37fef6
AgathePons <ponsagathe37@gmail.com>
2022-12-12 14:13:41 +0100

    update login method in user service to stock user info

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { UserDto } from '../dto/user-dto';
+import { User } from '../models/user';
 
 // TODO remove after wiring to backend
-const users: any[] = [
+const users: UserDto[] = [
   {
     id: 1,
     login: 'admin',
@@ -20,15 +22,21 @@ const users: any[] = [
 })
 export class UserService {
 
+  private _user: User | null = null;
+
   constructor() { }
 
   public login(formData: any): Observable<boolean> {
     console.log('user service login POUET', formData);
-    return of(
-      users.findIndex((user: any) => {
-        return user.login === formData.userLogin && user.password === formData.userPassword
-      }) !== -1
-    );
+    const userIndex: number = users.findIndex((user: UserDto) => user.login === formData.userLogin && user.password === formData.userPassword)
+    if (userIndex === -1) {
+      return of(false);
+    }
+    this._user = new User();
+    this._user.id = users[userIndex].id!;
+    this._user.login = users[userIndex].login;
+
+    return of(true);
   };
 
   public logout(): void {
