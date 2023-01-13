48efc45c1d532ef06f15a5f9ce3ac732c8fee86b
Fanny <tabaglio.fanny@gmail.com>
2023-01-13 14:31:37 +0100

    add answer-table

--- src/app/answer/components/answer-table/answer-table.component.ts
+++ src/app/answer/components/answer-table/answer-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Answer } from 'src/app/core/models/answer';
+import { AnswerService } from 'src/app/core/service/answer.service';
 
 @Component({
   selector: 'app-answer-table',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnswerTableComponent implements OnInit {
 
-  constructor() { }
+  public answers: Array<Answer> = [];
+
+  constructor(
+    private answerService: AnswerService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.answerService.findAll().subscribe((answers: Answer[]) => {
+      this.answers = answers;
+    })
   }
 
 }
