71f8e5e1611b26adf580ca1f4da1f335a1d20126
Agui <madjerx@hotmail.fr>
2023-01-14 21:49:23 +0100

    filter by birthdate

--- src/app/stagiaires/components/stagiaire-table/stagiaire-table.component.ts
+++ src/app/stagiaires/components/stagiaire-table/stagiaire-table.component.ts
@@ -17,6 +17,7 @@ export class StagiaireTableComponent implements OnInit {
   public stopDate: Date | null = null;
   public isDetailHidden: boolean = true;
   public selectedStagiaire: Stagiaire | null = null;
+  public stagiairesBirthDateYears: number[] = [];
   public bubbleConfig: any = {
     fontWeight: 'normal',
     backgroundColor: '#580883',
@@ -26,6 +27,7 @@ export class StagiaireTableComponent implements OnInit {
   public idOrder: boolean = false;
   searchFN = '';
   searchLN = '';
+  searchBD = '';
   filter = false;
 
   public constructor(
@@ -38,94 +40,100 @@ export class StagiaireTableComponent implements OnInit {
     this.stagiaireService.findAll().subscribe((stagiaires: Stagiaire[]) => {
       // this.stagiaires = stagiaires.sort((a,b) => a.getLastName() > b.getLastName() ? 1 : -1);
       this.stagiaires = stagiaires;
-      this.allStagiaires = this.stagiaires;
-      console.log(this.stagiaires);
+      this.stagiaires.forEach(stagiaire =>
+        this.stagiairesBirthDateYears.push(stagiaire.getBirthDate().getFullYear()))
+      this.stagiairesBirthDateYears.sort((a, b) => b - a)
+      this.stagiairesBirthDateYears = this.stagiairesBirthDateYears.filter(
+        (item,
+          index) => this.stagiairesBirthDateYears.indexOf(item) === index)      
+        this.allStagiaires = this.stagiaires;
+        console.log(this.stagiaires);
 
-    })
-    this.handleDetailService.isDetailHidden.subscribe((isDetailHidden: boolean) => {
-      this.isDetailHidden = isDetailHidden;
-    });
+      })
+      this.handleDetailService.isDetailHidden.subscribe((isDetailHidden: boolean) => {
+        this.isDetailHidden = isDetailHidden;
+      });
 
-    this.lastNameOrder = false;
-    this.idOrder = false;
-    this.firstNameOrder = false;
-  }
+      this.lastNameOrder = false;
+      this.idOrder = false;
+      this.firstNameOrder = false;
+    }
 
   public getVisibleStagiaireNumber(): number {
-    return this.stagiaireService.getStagiairesNumber(this.stopDate);
-  }
+      return this.stagiaireService.getStagiairesNumber(this.stopDate);
+    }
 
   public onEdit(stagiaire: Stagiaire): void {
-    console.log('Ding dong update :', stagiaire.getFirstName(), stagiaire.getLastName());
-    this.router.navigate(['/', 'stagiaire', 'update', stagiaire.getId()]);
-  }
+      console.log('Ding dong update :', stagiaire.getFirstName(), stagiaire.getLastName());
+      this.router.navigate(['/', 'stagiaire', 'update', stagiaire.getId()]);
+    }
 
   public onDelete(stagiaire: Stagiaire): void {
-    console.log(`Ici le component : Supprime ${stagiaire.getFirstName()} plizz`);
-    this.stagiaireService.removeOne(stagiaire).subscribe({
-      next: (_response: HttpResponse<any>) => {
-        this.stagiaires.splice(
-          this.stagiaires.findIndex((s: Stagiaire) => s.getId() === stagiaire.getId()),
-          1
-        )
-        // Here goes the snackbar
-      },
-      error: (error: any) => {
-        // Something went wrong, deal with it
-      }
-    });
-  }
+      console.log(`Ici le component : Supprime ${stagiaire.getFirstName()} plizz`);
+      this.stagiaireService.removeOne(stagiaire).subscribe({
+        next: (_response: HttpResponse<any>) => {
+          this.stagiaires.splice(
+            this.stagiaires.findIndex((s: Stagiaire) => s.getId() === stagiaire.getId()),
+            1
+          )
+          // Here goes the snackbar
+        },
+        error: (error: any) => {
+          // Something went wrong, deal with it
+        }
+      });
+    }
 
   public filterChanged(event: Date | null): void {
-    console.log(`Tut tut, change filter to ${event}`);
-    this.stopDate = event;
-  }
+      console.log(`Tut tut, change filter to ${event}`);
+      this.stopDate = event;
+    }
 
   public onClick(stagiaire: Stagiaire): void {
-    this.router.navigate(['/', 'stagiaire', stagiaire.getId()]);
-    /* this.handleDetailService.setIsDetailHidden(false);
-    this.selectedStagiaire = stagiaire; */
-  }
+      this.router.navigate(['/', 'stagiaire', stagiaire.getId()]);
+      /* this.handleDetailService.setIsDetailHidden(false);
+      this.selectedStagiaire = stagiaire; */
+    }
 
   public onDetailClose(event: boolean): void {
-    this.isDetailHidden = event;
+      this.isDetailHidden = event;
 
-  }
+    }
 
   public sortByFirstName(stagiaire: Stagiaire[]): void {
-    this.firstNameOrder = true
+      this.firstNameOrder = true
     this.stagiaireService.sortByFirstName(stagiaire)
-  }
+    }
   public sortByFirstNameDesc(stagiaire: Stagiaire[]): void {
-    this.firstNameOrder = false
+      this.firstNameOrder = false
     this.stagiaireService.sortByFirstNameDesc(stagiaire)
-  }
+    }
 
   public sortById(stagiaire: Stagiaire[]): void {
-    this.idOrder = true
+      this.idOrder = true
     this.stagiaireService.sortById(stagiaire)
-  }
+    }
   public sortByIdDesc(stagiaire: Stagiaire[]): void {
-    this.idOrder = false
+      this.idOrder = false
     this.stagiaireService.sortByIdDesc(stagiaire)
-  }
+    }
 
   public sortByLastName(stagiaire: Stagiaire[]): void {
-    console.log('call sortBylastName');
+      console.log('call sortBylastName');
 
-    this.lastNameOrder = true
+      this.lastNameOrder = true
     this.stagiaireService.sortByLastName(stagiaire)
-  }
+    }
   public sortByLastNameDesc(stagiaire: Stagiaire[]): void {
-    console.log('call sortBylastNameDesc');
+      console.log('call sortBylastNameDesc');
 
-    this.lastNameOrder = false
+      this.lastNameOrder = false
     this.stagiaireService.sortByLastNameDesc(stagiaire)
-  }
+    }
 
   searchByLastName(value: string): void {
-    // this.searchFN = '';
-    if (this.searchFN !== '') {
+    this.searchBD = '';
+      if(this.searchFN !== '') {
       this.stagiaires = this.allStagiaires.filter((val) =>
         val.getLastName().toLowerCase().includes(value.toLowerCase())
       ).filter((val) =>
@@ -139,6 +147,7 @@ export class StagiaireTableComponent implements OnInit {
   }
 
   searchByFirstName(value: string): void {
+    this.searchBD = '';
     if (this.searchLN !== '') {
       this.stagiaires = this.allStagiaires.filter((val) =>
         val.getFirstName().toLowerCase().includes(value.toLowerCase())
@@ -151,15 +160,21 @@ export class StagiaireTableComponent implements OnInit {
       );
     }
   }
+  searchByBirthDate(value: string): void {
+    this.searchLN = '';
+    this.searchFN = '';
+      this.stagiaires = this.allStagiaires.filter((val) =>
+        val.getBirthDate().getFullYear().toString().includes(value))
+  
+  }
   changeBooleanState() {
     console.log('changebutton call');
-    
-    if (this.filter) 
-    { this.filter = false}
+
+    if (this.filter) { this.filter = false }
     else {
       this.filter = true
     }
-    
+
 
   }
 }
