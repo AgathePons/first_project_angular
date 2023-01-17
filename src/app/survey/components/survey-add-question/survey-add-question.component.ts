53bebad4635f1ab186df1aa46efee9db89ed8a58
Fanny <tabaglio.fanny@gmail.com>
2023-01-17 10:27:54 +0100

    addquestionsinSurvey implemented

--- src/app/survey/components/survey-add-question/survey-add-question.component.ts
+++ src/app/survey/components/survey-add-question/survey-add-question.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatSelectionListChange } from '@angular/material/list';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Question } from 'src/app/core/models/question';
 import { Survey } from 'src/app/core/models/survey';
@@ -50,6 +52,48 @@ export class SurveyAddQuestionComponent implements OnInit {
             });
           });
       });
+  }
+
+  public onListSelectionChange(obj: MatSelectionListChange): void {
+    const seletedObj: Array<any> = obj.source.selectedOptions.selected
+
+    this.questionsToAdd = seletedObj.map(questionInSelection => questionInSelection.value);
+    console.log('>> stagiaire to add:', this.questionsToAdd);
+  
+  }
+
+  public questionsToIDArray(): Array<number> {
+    this.questionsToAdd.forEach((question) => {
+      this.questionsIdToAdd.push(question.getId())
+    })
+    return this.questionsIdToAdd
+  }
+
+  public onAddManyQuestions(): void {
+    console.log('onAddManyQuestions');
+    this.surveyService.addManyQuestions(this.survey.getId(), this.questionsToIDArray()).subscribe((survey: Survey) => {
+      this.survey = survey;
+      this.router.navigate(['/', 'survey', 'detail', this.survey.getId()]);
+    });
+  }
+
+  public onBackButton(): void {
+    this.location.back();
+  }
+
+  changeBooleanState() {
+    console.log('changebutton call');
+
+    if (this.filter) { this.filter = false }
+    else {
+      this.filter = true
+    }
+  }
+
+  resetFilter(): void {
+    this.filterFN = '';
+    this.filterLN = '';
+    this.questions = this.allQuestions
   }
 
 }
