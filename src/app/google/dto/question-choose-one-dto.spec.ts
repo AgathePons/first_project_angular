746d4c9cd1540151ed52b3e4e1e45ce67c7cfb18
AgathePons <ponsagathe37@gmail.com>
2023-01-18 15:56:52 +0100

    add a question-choose-one-dto that extends question-dto

---
+++ src/app/google/dto/question-choose-one-dto.spec.ts
@@ -0,0 +1,7 @@
+import { QuestionChooseOneDto } from './question-choose-one-dto';
+
+describe('QuestionChooseOneDto', () => {
+  it('should create an instance', () => {
+    expect(new QuestionChooseOneDto()).toBeTruthy();
+  });
+});
