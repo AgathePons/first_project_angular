e6fb3fbae5993921cf6919f95f39476bab92b554
Fanny <tabaglio.fanny@gmail.com>
2023-01-23 10:48:28 +0100

    change answer input & updateDto

--- src/app/answer/dto/answer-update-dto.ts
+++ src/app/answer/dto/answer-update-dto.ts
@@ -3,10 +3,12 @@ export class AnswerUpdateDto {
 
     public id?: number;
     public text: string = '';
+    public orderInQuestion: number = 0;
 
-    public constructor(id:number , text: string) {
+    public constructor(id:number , text: string, orderInQuestion: number) {
         this.text = text
         this.id = id
+        this.orderInQuestion = orderInQuestion;
 
 
       }}
