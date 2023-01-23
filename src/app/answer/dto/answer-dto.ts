abd8084ff94ed864bb1fd6f78212369e4c03de54
Fanny <tabaglio.fanny@gmail.com>
2023-01-23 10:40:43 +0100

    add order in answer entity & dto

--- src/app/answer/dto/answer-dto.ts
+++ src/app/answer/dto/answer-dto.ts
@@ -1,6 +1,7 @@
 export class AnswerDto {
     public id?: number;
     public text: string = '';
+    public orderInQuestion: number = 0;
 
     public constructor(formValues: any) {
         Object.assign(this, formValues);
