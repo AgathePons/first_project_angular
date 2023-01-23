4151f0e25df447844367ad7b8dd003933a7934fe
Agui <madjerx@hotmail.fr>
2023-01-23 11:08:44 +0100

    improve method if no question return max order 0

--- src/app/survey/components/survey-details/survey-details.component.ts
+++ src/app/survey/components/survey-details/survey-details.component.ts
@@ -316,11 +316,14 @@ public saveQuestionText(id: number, type: string, orderInSurvey: number) {
 public lastOrderInSurvey(): number {
   let orderQuestionArray: number[] = [];
 
-  this.survey.getQuestions().forEach( question => {
-    orderQuestionArray.push(question.getOrderInSurvey())
-  })
-
-  return Math.max(...orderQuestionArray!) + 1 
+  if (this.survey.getQuestions().length !== 0 ) {
+    this.survey.getQuestions().forEach( question => {
+      orderQuestionArray.push(question.getOrderInSurvey())
+    })
+  
+    return Math.max(...orderQuestionArray!) + 1 
+  }
+  return 0
 }
 
 }
