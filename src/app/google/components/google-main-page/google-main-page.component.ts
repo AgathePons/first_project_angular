35fb01992c1f6d5bd44e46a2cfb1097de7429428
AgathePons <ponsagathe37@gmail.com>
2023-01-24 15:28:06 +0100

    when form created, add an "open form in new tab" button + a "copy link to clipborad btn

--- src/app/google/components/google-main-page/google-main-page.component.ts
+++ src/app/google/components/google-main-page/google-main-page.component.ts
@@ -3,6 +3,7 @@ import { Survey } from 'src/app/core/models/survey';
 import { GoogleService } from 'src/app/core/service/google.service';
 import { SurveyService } from 'src/app/core/service/survey.service';
 import { UserService } from 'src/app/user/services/user.service';
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-google-main-page',
@@ -13,18 +14,20 @@ export class GoogleMainPageComponent implements OnInit {
 
   public hasUser: boolean = false;
   public hasGoogleToken: boolean = false;
-  public googleFolderResponse: any | null = null;
   public googleFolderId: string = '';
   public googleFormId: string = '';
   public googleForm: any = null;
-  public surveys: Array<Survey> = [];
 
-  @ViewChild('linkCell') linkCell: ElementRef | undefined;
+  private googleFolderResponse: any | null = null;
+  //private surveys: Array<Survey> = [];
+
+  public surveysWithLink: Array<any> = [];
 
   constructor(
     private userService: UserService,
     private googleService: GoogleService,
     private surveyService: SurveyService,
+    private clipboard: Clipboard,
   ) { }
 
   ngOnInit(): void {
@@ -60,9 +63,11 @@ export class GoogleMainPageComponent implements OnInit {
       }
       // find surveys list
       this.surveyService.findAll().subscribe((surveys: Survey[]) => {
-        surveys.forEach((survey) => {
+        surveys.forEach((survey, index) => {
           this.surveyService.findOne(survey.getId()).subscribe((survey: Survey) => {
-            this.surveys.push(survey);
+            //this.surveys.push(survey);
+            this.surveysWithLink.push(survey);
+            this.surveysWithLink[index].link = '';
           })
         })
       });
@@ -78,11 +83,10 @@ export class GoogleMainPageComponent implements OnInit {
           (googleFormResponse: any) => {
             this.googleForm = googleFormResponse;
             console.log('url:', this.googleForm.responderUri);
-            console.log(this.linkCell);
-
-            if(this.linkCell) {
-              this.linkCell.nativeElement.textContent = this.googleForm.responderUri;
-            }
+            // Set the link into the cell
+            const surveyId = survey.getId();
+            const surveywithLink = this.surveysWithLink.filter(survey => survey.id === surveyId)[0];
+            surveywithLink.link = this.googleForm.responderUri;
           }
         );
         this.googleService.deleteFirstItem(this.googleFormId).subscribe(
@@ -92,6 +96,14 @@ export class GoogleMainPageComponent implements OnInit {
         );
       }
     );
+  }
+
+  public goToForm(link: string): void {
+    window.open(link, "_blank");
+  }
+
+  public copyToClipboard(link: string): void {
+    this.clipboard.copy(link);
   }
 
   public onDeleteGoogleToken(): void {
