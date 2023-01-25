a90971f6f8f93162655e8a4f49a9a6e0060df51f
Agui <madjerx@hotmail.fr>
2023-01-25 10:14:29 +0100

    put input on token component to go back when not on sending mail page

--- src/app/user/google/google-token-form/google-token-form.component.ts
+++ src/app/user/google/google-token-form/google-token-form.component.ts
@@ -1,7 +1,7 @@
 import { Location } from '@angular/common';
-import { Component, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { UserService } from '../../services/user.service';
 
@@ -15,6 +15,7 @@ export class GoogleTokenFormComponent implements OnInit, OnDestroy {
   public googleTokenForm!: FormGroup;
   private _subscription!: Subscription;
   public error!: string;
+  @Input() public fromMailPage: boolean = false;
   @Output() public tokkenGoogleActivated: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(
@@ -43,11 +44,17 @@ export class GoogleTokenFormComponent implements OnInit, OnDestroy {
 
   public onSetGoogleToken(): void {
     console.log('Set Google token:', this.googleTokenForm.value);
+    
 
     this._subscription = this.userService.setGoogleToken(this.googleTokenForm.value)
       .subscribe((googleAuthentificated: boolean) => {
         if (googleAuthentificated) {
-          this.tokkenGoogleActivated.emit(true)
+          if (this.fromMailPage === true) {
+            this.tokkenGoogleActivated.emit(true)
+
+          } else {
+            this.router.navigate(['/','googlepage'])
+          }
         } else {
           this.error = 'invalid token'
         }
